9d0fe7bbdb09520d983e78004834cb957c4ebdf8
Mahmud Hossain <mahmud.paiira@gmail.com>
2026-07-17 12:34:15 +0600

    Reapply "fix: use unicode escapes in utils.py for server encoding safety"
    This reverts commit e408d1c97901f9a5de7e0bdde64e245b24101687.

--- backend/apps/common/utils.py
+++ backend/apps/common/utils.py
@@ -1,4 +1,5 @@
-BANGLA_DIGITS = '০১২৩৪৫৬৭৮৯'
+BANGLA_DIGITS = '\u09e6\u09e7\u09e8\u09e9\u09ea\u09eb\u09ec\u09ed\u09ee\u09ef'
+
 
 def to_english_digits(value):
     """Convert Bengali/Arabic-Indic digits to English digits."""
